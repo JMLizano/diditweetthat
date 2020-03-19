18e31b3e70504e9babaaa7a59e70ef946f8571cd
Chema <josemaria.lizano@lynxanalytics.com>
2020-03-19 15:57:03 +0100

    First working version of the getTimeline

--- src/test/scala/diditweethat/TwitterSuite.scala
+++ src/test/scala/diditweethat/TwitterSuite.scala
@@ -1,21 +1,36 @@
 package diditweethat
 
-import scala.concurrent.Future
-
-import org.scalatest.funsuite.AsyncFunSuite
-
+import scala.concurrent.{Await, Future}
+import org.scalatest.funsuite.{AnyFunSuite, AsyncFunSuite}
 import diditweetthat.Twitter
 import diditweetthat.Twitter.simpleTweet
 
 
 class TwitterSuite extends AsyncFunSuite {
 
-  test("getTimeline should return tweets for the user") {
+  test("getTimeline should return some tweets for the user") {
     val timeline: Future[Seq[simpleTweet]] = Twitter.getTimeline("twitterapi")
     timeline.map { tweets =>
-      println(tweets.minBy(tweet => tweet.created_at))
-      assert(tweets.length > 0)
+      assert(tweets.nonEmpty)
     }
+  }
+
+  test("TimelineStream should return all tweets for the user") {
+    import scala.concurrent.duration._
+
+    val timeline: LazyList[Future[Seq[simpleTweet]]] = Twitter.TimelineStream("twitterapi")
+    val firstTweet = timeline
+      .map(tweetChunk => Await.result(tweetChunk, Duration.Inf))
+      .fold(Seq.empty) { (tweetChunk: Seq[simpleTweet], nextTweetChunk: Seq[simpleTweet]) =>
+        tweetChunk ++ nextTweetChunk
+      }.minBy(tweet => tweet.created_at)
+    // This is not the first tweet for twitterapi, but seems like the standard API
+    // does not go further back in time
+    assert(firstTweet.created_at.toString == "2009-05-08T18:54:09Z")
+  }
+
+  test("deleteTweet should delete the specified tweet") {
+    Twitter.deleteTweet()
   }
 
 }
